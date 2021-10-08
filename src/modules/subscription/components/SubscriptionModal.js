7afe41eb8c98f97bcecb6aaee94f1c81e6e285a0
comeonandroid <comeonandroid@gmail.com>
2021-10-08 22:25:19 +0300

    add subscription form

--- src/modules/subscription/components/SubscriptionModal.js
+++ src/modules/subscription/components/SubscriptionModal.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Button,
   Flex,
-  Link,
-  Heading,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalBody,
   ModalCloseButton,
-  Text,
   FormControl,
-  Switch,
   FormLabel,
   Input,
   Select
 } from '@chakra-ui/react'
+
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const FormInput = ({...props}) => (<Input
   borderRadius="15px"
@@ -40,7 +39,19 @@ const FormSelect = ({children,...props}) => (<Select
     {children}
   </Select>)
 
-export default function SubscriptionModal({ isOpen, onClose }) {
+export default function SubscriptionModal({ isOpen, onClose, onSubmit }) {
+  const formik = useFormik({
+    initialValues: {},
+    validationSchema: Yup.object({
+      'name': Yup.string()
+        .required('Required filed'),
+      'amount': Yup.string()
+        .required('Required filed'),
+    }),
+    onSubmit: values => {
+      onSubmit(values)
+    }
+  })
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered size='md'>
@@ -75,36 +86,57 @@ export default function SubscriptionModal({ isOpen, onClose }) {
             background="transparent"
             p="12px"
           >
-            <FormControl>
-              <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
-                Name
-              </FormLabel>
-              <FormInput placeholder="Name" />
-              <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
-                Amount
-              </FormLabel>
-              <FormInput placeholder="Amount" />
-              <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
-                Period
-              </FormLabel>
-              <Select placeholder="Period" >
-                <option value="300">5 minutes</option>
-                <option value="900">15 minutes</option>
-                <option value="1800">30 minutes</option>
-              </Select>
-              <Button
-                fontSize="12px"
-                type="submit"
-                colorScheme='main'
-                w="100%"
-                h="45"
-                mb="20px"
-                color="white"
-                mt="20px"
-              >
-                CREATE
-              </Button>
-            </FormControl>
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl isInvalid={formik.errors['name'] && formik.touched['name']}>
+                <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                  Name
+                </FormLabel>
+                <FormInput
+                  name={'name'}
+                  id={'name'}
+                  value={formik.values['name']}
+                  onChange={formik.handleChange}
+                  placeholder="Name" />
+              </FormControl>
+              <FormControl isInvalid={formik.errors['amount'] && formik.touched['amount']}>
+                <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                  Amount
+                </FormLabel>
+                <FormInput
+                  name={'amount'}
+                  id={'amount'}
+                  value={formik.values['amount']}
+                  onChange={formik.handleChange}
+                  placeholder='Amount' />
+              </FormControl>
+              <FormControl isInvalid={formik.errors['period'] && formik.touched['period']}>
+                <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                  Period
+                </FormLabel>
+                <FormSelect 
+                  name={'period'}
+                  id={'period'}
+                  value={formik.values['period']}
+                  onChange={formik.handleChange} 
+                  placeholder="Period" >
+                  <option selected value="300">5 minutes</option>
+                  <option value="900">15 minutes</option>
+                  <option value="1800">30 minutes</option>
+                </FormSelect>
+               </FormControl>
+               <Button
+                  fontSize="12px"
+                  type="submit"
+                  colorScheme='main'
+                  w="100%"
+                  h="45"
+                  mb="20px"
+                  color="white"
+                  mt="20px"
+                >
+                  CREATE
+                </Button>
+            </form>
           </Flex>
         </Flex>
         </ModalBody>
@@ -112,3 +144,58 @@ export default function SubscriptionModal({ isOpen, onClose }) {
     </Modal>
   )
 }
+
+// function InviteFormSingle ({ values, isLoading, isInvited, isDisabled, onInvite }) {
+//   const [name, value] = Object.entries(values)[0]
+//   const [isSubmitting, setIsSubmitting] = useState(false)
+
+//   const formik = useFormik({
+//     initialValues: { [name]: '' },
+//     validationSchema: Yup.object({
+//       [name]: Yup.string()
+//         .email('Please enter valid email address.')
+//         .required('Please enter your email address')
+//     }),
+//     onSubmit: values => {
+//       setIsSubmitting(true)
+//       onInvite(name, values[name])
+//     }
+//   })
+
+//   return (
+//     <>
+//       <form onSubmit={formik.handleSubmit}>
+//         <FormControl width="100%" isInvalid={formik.errors[name] && formik.touched[name]}>
+//           <FormLabel htmlFor={name}>Email</FormLabel>
+//           <TextInput
+//             focusBorderColor="primary"
+//             size="sm"
+//             rounded="4px"
+//             name={name}
+//             id={name}
+//             value={formik.values[name]}
+//             onChange={formik.handleChange}
+//             isDisabled={(isSubmitting && isLoading) || isInvited || isDisabled}
+//             isReadOnly={isInvited}
+//             placeholder={t('inviteModal.Enter email address')} />
+//           <FormErrorMessage mb="1">{formik.errors[name]}</FormErrorMessage>
+//         </FormControl>
+
+//         <Box textAlign="center" width="100%" mt="6">
+//           <Button
+//             type="submit"
+//             size="md"
+//             variantColor="primary"
+//             minWidth="136px"
+//             minHeight="38px"
+//             borderRadius="8px"
+//             fontWeight="medium"
+//             isDisabled={isDisabled}
+//             isLoading={isSubmitting && isLoading}>
+//             {t('Invite')}
+//           </Button>
+//         </Box>
+//       </form>
+//     </>
+//   )
+// }
