c611d5940484399db8fc4066b6ce2f0a1ba3a90e
comeonandroid <comeonandroid@gmail.com>
2021-10-09 19:03:42 +0300

    fast fix

--- src/modules/wallet/index.js
+++ src/modules/wallet/index.js
@@ -2,6 +2,3 @@ export { default as AccountModal } from './components/AccountModal'
 export { default as ConnectButton } from './components/ConnectButton'
 export { default as Identicon } from './components/Identicon'
 export { default as Asset } from './components/Asset'
-
-export { default as useAddress } from './hooks/useAddress'
-export { default as useHistory } from './hooks/useHistory'
