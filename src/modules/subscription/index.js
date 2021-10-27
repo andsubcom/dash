6987223c131983a8c2957ad101fac05617597f54
comeonandroid <comeonandroid@gmail.com>
2021-10-27 18:33:00 +0300

    fix deployment bug

--- src/modules/subscription/index.js
+++ src/modules/subscription/index.js
@@ -2,5 +2,4 @@ export * from './hooks/useSubscription'
 
 export { default as SubscriptionModal } from './components/SubscriptionModal'
 export { default as WithdrawModal } from './components/WithdrawModal'
-export { default as WithdrawWidget } from './components/WithdrawWidget'
 export { default as ProductItem } from './components/ProductItem'
